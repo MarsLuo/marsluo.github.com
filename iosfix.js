b3c1e18b67e33d847941b78c33f87c17345de542
骆杨 <yluo@thinkjoy.cn>
2016-03-14 11:04:22 +0800

    更新热更新代码

--- iosfix.js
+++ iosfix.js
@@ -37,9 +37,9 @@ defineClass('NetWorkClient', {}, {
         };
 
         NetWorkClient.postRequest_parameters_serviceType_NeedPrompt_WaitPrompt_ShouldDismissPrompt_success_failure(path, parameter, 3, YES, NO, YES, block('id', function(dataObject) {
-            success(dataObject.toJS());
+            // success(dataObject.toJS());
         }), block('id', function(dataObject) {
-            failure();
+            // failure();
         }));
     },
 });
