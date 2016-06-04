d258b86d223d3c80e43b0a2a145f9cb5146c0dd4
骆杨 <yluo@thinkjoy.cn>
2016-06-04 15:06:50 +0800

    update iOS fix

--- iosfix.js
+++ iosfix.js
@@ -2,8 +2,9 @@ require('PublicWebViewController'); // 1.7.0 增加成长足迹缺省页
 defineClass('MeViewController', {
     gotoZLChildTrackViewController: function() {
 
+ 		var requestUrlStr = NSString.stringWithFormat("%@:%@%", HostDomain, "3001/cmw/growPoints/growPoints");
         var webViewController = PublicWebViewController.alloc().initWithNibName_bundle("PublicWebViewController", null);
-        webViewController.setRequestUrlStr("http://www.baidu.com");
+        webViewController.setRequestUrlStr(requestUrlStr);
         webViewController.setRequestType("成长足迹");
         self.navigationController().pushViewController_animated(webViewController, YES);
     },
