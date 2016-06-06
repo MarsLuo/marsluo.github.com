0557410762857e1f06320c3d72acf5172b72e97e
骆杨 <yluo@thinkjoy.cn>
2016-06-06 12:06:17 +0800

    修正分享问题。

--- iosfix.js
+++ iosfix.js
@@ -1,10 +1,32 @@
-require('PublicWebViewController,NSString'); // 1.7.0 增加成长足迹缺省页
-defineClass('MeViewController', {
-    gotoZLChildTrackViewController: function() {
+require('NSString'); // 修正分享问题
+defineClass('ZLPrivilegeCenterViewController', {
+    webView_shouldStartLoadWithRequest_navigationType: function(webView, request, navigationType) {
+        var requestURL = request.URL();
 
-        var webViewController = PublicWebViewController.alloc().initWithNibName_bundle("PublicWebViewController", null);
-        webViewController.setRequestUrlStr("http://dev.imzhiliao.com:3001/cmw/growPoints/growPoints");
-        webViewController.setRequestType("成长足迹");
-        self.navigationController().pushViewController_animated(webViewController, YES);
+        if (requestURL.scheme().isEqualToString(CICADA_URL_SCHEME)) {
+
+            if (requestURL.host().isEqualToString(CICADA_CICADA)) {
+
+                if (requestURL.path().isEqualToString(CICADA_CICADA_PAGE_GOPAGE)) {
+                    // 页面跳转
+                    var paramString = requestURL.absoluteString().componentsSeparatedByString("?").lastObject();
+                    var queryDit = paramString.URLDecodedString().getQueryDict();
+                    var pageNameValue = queryDit[CICADA_P_VIEWNAME];
+
+                    if (pageNameValue.isEqualToString(CICADA_V_Pay)) { //支付相关   
+                        var sloganJsonStr = queryDit[CICADA_P_SLOGAN];
+                        var sloganDic = NSString.jsonObjectWithNSString(sloganJsonStr);
+                        self.gotoZLBuyVipController(sloganDic);
+                    }
+                }
+            }
+            return NO;
+        }
+
+        self.setZlShareUrl(NSString.stringWithFormat("%", webView.request().URL()));
+
+        self.setZlLeaveRequest(request);
+
+        return YES;
     },
 });
