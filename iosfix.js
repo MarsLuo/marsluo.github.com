826ee02bc0a1f020cddfeeee098c4b58739323a1
骆杨 <yluo@thinkjoy.cn>
2016-06-30 10:06:06 +0800

    update ios fix

--- iosfix.js
+++ iosfix.js
@@ -1,31 +1,33 @@
-require('NSString'); // 修正分享问题 1.8.0.197
-defineClass('ZLPrivilegeCenterViewController', {
-    webView_shouldStartLoadWithRequest_navigationType: function(webView, request, navigationType) {
-        var requestURL = request.URL();
-
-        if (requestURL.scheme().isEqualToString('cicada')) {
-
-            if (requestURL.host().isEqualToString('cicada')) {
-
-                if (requestURL.path().isEqualToString('/page/goPage')) {
-                    // 页面跳转
-                    var paramString = requestURL.absoluteString().componentsSeparatedByString("?").lastObject();
-                    var queryDit = paramString.URLDecodedString().getQueryDict();
-                    var pageNameValue = queryDit.objectForKey("viewName");
-
-                    if (pageNameValue.isEqualToString('pay')) { //支付相关   
-                        var sloganJsonStr = queryDit.objectForKey("slogan");
-                        var sloganDic = NSString.jsonObjectWithNSString(sloganJsonStr);
-                        self.gotoZLBuyVipController(sloganDic);
-                    }
-                }
-            }
-            return NO;
+require('NSString,NSNumber');  // 修正园长不能正确查看接送信息的问题
+defineClass('ZLDirectorAssistantViewController', {
+    updateUIMethodWithkindergartenCount: function() {
+        for (var i = 0; i < self.kindergartenCountArray().count(); i++) {
+            var zlDirectorKG = self.kindergartenCountArray[i]();
+            _schoolIdArray.addObject(NSString.stringWithFormat("%lld", (long long) zlDirectorKG.schoolId()));
+            _schoolNameArray.addObject(zlDirectorKG.schoolName());
+            _levelArr.addObject(NSString.stringWithFormat("%ld", (long) zlDirectorKG.level()));
+            _hasCardMachineArray.addObject(NSNumber.numberWithBool(zlDirectorKG.hasCardMachine()));
+        }
+        if (_schoolNameArray.count() == 1) {
+            _ui_btnImage.setHidden(YES);
+            _selectTableView.setHidden(YES);
+            _selectBtn.setUserInteractionEnabled(NO);
+            _hasCardMachine = _hasCardMachineArray.firstObject().boolValue();
         }
 
-        self.setZlShareUrl(NSString.stringWithFormat("%@", requestURL));
-        self.setZlLeaveRequest(request);
-
-        return YES;
+        if (_schoolNameArray.count() < 4) {
+            _selectViewHeight.setConstant(_schoolNameArray.count() * 40);
+        } else {
+            _selectViewHeight.setConstant(40 * 4);
+        }
+        self.selectTableView().reloadData();
+        if (self.schoolNameArray().count() > 0) {
+            self.ui_titleLabel().setText(self.schoolNameArray().firstObject());
+        }
+        if (self.schoolIdArray().count() > 0) {
+            self.setSchoolIdStr(self.schoolIdArray().firstObject());
+        }
+        self.queryTeacherAndChildAttendanceBySchoolId();
+        self.configDirectorFunction(self.levelArr().firstObject().integerValue());
     },
 });
